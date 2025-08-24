11a7bb39ecc3bf0e502524d6f5ed8f0cc58e8cd3
Your Name <your_email@example.com>
2025-08-24 16:31:43 +0530

    Added the arithmetic operation

---
+++ arithmetic-operation.py
@@ -0,0 +1,37 @@
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Example
+
+
+# Print the following:
+
+# 8
+# -2
+# 15
+
+
+
+# solution:- 
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+
+#     # for sum
+#     print (a+b)
+
+#     # for Difference
+#     print(a-b)
+
+#     # Product
+#     print(a*b)
+
+#     output:- 
+
+#     8
+#     -2
+#     15
+
